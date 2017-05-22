38499398cf0d7c481a440ac0b9a9fd0a4d5af7be
Ramesh Polishetti <rpolishe@appss-MacBook-Pro-3.local>
2017-05-22 17:05:51 +0530

    added custom activity

--- src/demoapp/imports.go
+++ src/demoapp/imports.go
@@ -4,6 +4,8 @@ import (
 
 	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
+	_ "github.com/TIBCOSoftware/flogo-contrib/model/simple"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
+	_ "github.com/rameshpolishetti/demoActivity"
 
 )
